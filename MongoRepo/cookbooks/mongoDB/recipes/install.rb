4783b2795813f466c94e64a9df3109027c91e54b
Subhi Andrews <subhia@gmail.com>
2016-06-21 19:32:15 +0000

    install recipe for MongoDB

--- MongoRepo/cookbooks/mongoDB/recipes/install.rb
+++ MongoRepo/cookbooks/mongoDB/recipes/install.rb
@@ -3,3 +3,15 @@
 yum_package 'MongoDB Install' do
 	package_name mongodb-org
 done
+yum_package "MondgoDB Server Install" do
+	package_name mongodb-org-server 
+done
+yum_package "MondgoDB Mongos Install" do
+	package_name mongodb-org-mongos 
+done
+yum_package "MondgoDB Shell Install" do
+	package_name mongodb-org-shell 
+done
+yum_package "MondgoDB Tools Install" do
+	package_name mongodb-org-tools 
+done
